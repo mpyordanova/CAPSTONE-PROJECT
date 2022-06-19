747409b9bd42c13e4477224189e161cdd96cf1c5
Mariela <yordanova.mariela@gmail.com>
2022-06-19 14:36:40 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Pictures from './Pictures'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="container">
+
+        <h2 className='beach'>Beach Relaxation</h2>
+        <h2 className='mountain'>Mountain Escapes</h2>
+        <h2 className='city'>Urban Exploration</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+{/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header =() => {
+    return(
+        <header className='header.container'>
+            <div className="headrer-left">
+            <h2>From Ocean Blues to Mountain Hues</h2>
+            
+            </div>
+        
+
+
+        </header>
+    )
+}
